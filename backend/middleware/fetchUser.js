b4040499d6f133925651e4584fa2f9c58dfe56c2
sachin <sachinsiddhu112@gmail.com>
2024-08-24 10:05:09 +0530

    some ui changes

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -5,7 +5,7 @@ export const fetchUser = async (req, res, next) => {
     const authToken = req.header('authToken');
     //no auth token ,means not authenticated.
     if (!authToken) {
-        res.status(401).json({ error: "Please authenticate yourself." });
+        res.status(401).json({ error: "Please signup yourself." });
         return;
     }
     try {
